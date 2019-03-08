43ccc8fe7852923275351b97c9ed1beb3ce04861
Mac <teagueam@ymail.com>
2019-03-07 19:06:17 -0800

    new

--- main.js
+++ main.js
@@ -4,89 +4,92 @@ AOS.init({
   once: true
 });
 
-$(document).ready(function () {
-
-  const beaches = [{
-    name: "Zanpa Beach",
-    description: "This beach has it all, jet skies, kayaks, water tubing and more! Relax on the beach or rent a chair and umbrella. "
-  }, {
-    name: "Emerald Beach",
-    description: "Info"
-  }, {
-    name: "Sesoko Beach",
-    description: "info."
-  }, {
-    name: "Manza Beach",
-    description: "Located at ANA Intercontinental Manza Beach Resort this family friendly beach has rentable water activites, including a fun playground for kids and adults!"
-  }, {
-    name: "Kabira Beach",
-    description: "Any input I want"
-  }, {
-    name: "Okuma Beach",
-    description: "add more info"
-  }];
+//$(document).ready(function () {
+document.addEventListener("DOMContentLoaded",
+  function (event) {
 
 
-  const findBeach = function (beachName) {
-    return beaches.find(function (beach) {
-      return beachName === beach.name
-    });
-  }
+    const beaches = [{
+      name: "Zanpa Beach",
+      description: "This beach has it all, jet skies, kayaks, water tubing and more! Relax on the beach or rent a chair and umbrella. "
+    }, {
+      name: "Emerald Beach",
+      description: "Info"
+    }, {
+      name: "Sesoko Beach",
+      description: "info."
+    }, {
+      name: "Manza Beach",
+      description: "Located at ANA Intercontinental Manza Beach Resort this family friendly beach has rentable water activites, including a fun playground for kids and adults!"
+    }, {
+      name: "Kabira Beach",
+      description: "Any input I want"
+    }, {
+      name: "Okuma Beach",
+      description: "add more info"
+    }];
 
 
-  // Register click event for gallery items
-  $('.item').click(function () {
+    const findBeach = function (beachName) {
+      return beaches.find(function (beach) {
+        return beachName === beach.name
+      });
+    }
 
 
-    let beachName = $(this).find('.description-heading').text();
-    let getBeachInfo = findBeach(beachName);
-    console.log(getBeachInfo);
+    // Register click event for gallery items
+    $('.item').click(function () {
 
 
-    $('#description').text(getBeachInfo.description);
+      let beachName = $(this).find('.description-heading').text();
+      let getBeachInfo = findBeach(beachName);
+      console.log(getBeachInfo);
+
+
+      $('#description').text(getBeachInfo.description);
+    })
+
+    //});
+
+
+
+    // email subscription
+    function validateEmail(email) {
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+
+    function validate() {
+      //let $result = $("#result");
+      //let email = $("#email").val();
+      //$result.text("");
+      let result = document.querySelector("#result")
+      let email = document.querySelector("#email").value
+      result.innerHTML = ""
+
+
+      if (validateEmail(email)) {
+        result.innerHTML = (" Thanks for stopping by!");
+        result.style.color = "green"
+        //$result.text(" Thanks for stopping by!");
+        //$result.css("color", "green");
+      } else {
+        result.style.color = "red"
+        result.innerHTML = "oops something went wrong:("
+        //$result.text(" oops something went wrong:(");
+        //$result.css("color", "red");
+      }
+      return false;
+    }
+
+    //$("#validate").bind("click", validate);
+
+    let validateButton = document.querySelector('#validate');
+    validateButton.addEventListener('submit', function (event) {
+      event.preventDefault()
+      validate()
+    })
   })
-
-});
-
-
-
-// email subscription
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
-
-function validate() {
-  //let $result = $("#result");
-  //let email = $("#email").val();
-  //$result.text("");
-  let result = document.querySelector("#result")
-  let email = document.querySelector("#email").value
-  result.innerHTML = ""
-
-
-  if (validateEmail(email)) {
-    result.innerHTML = (" Thanks for stopping by!");
-    result.style.color = "green"
-    //$result.text(" Thanks for stopping by!");
-    //$result.css("color", "green");
-  } else {
-    result.style.color = "red"
-    result.innerHTML = "oops something went wrong:("
-    //$result.text(" oops something went wrong:(");
-    //$result.css("color", "red");
-  }
-  return false;
-}
-
-//$("#validate").bind("click", validate);
-
-let validateButton = document.querySelector('#validate');
-validateButton.addEventListener('submit', function (event) {
-  event.preventDefault()
-  validate()
-})
-
 
 
 
