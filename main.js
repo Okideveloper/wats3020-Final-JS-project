33ae287f0eeda8b237781ff5bdc566dae0cf6af1
Mac <teagueam@ymail.com>
2019-03-14 18:25:26 -0700

    new code

--- main.js
+++ main.js
@@ -11,26 +11,26 @@ document.addEventListener("DOMContentLoaded",
 
     const beaches = [{
       name: "Manza Beach",
-      description: "Located at ANA Intercontinental Manza Beach Resort with rentables"
+      description: "Located at ANA Intercontinental Manza Beach Resort with many water rentals including access to the large and popular playground on the water."
     }, {
       name: "Okuma Beach",
-      description: ""
+      description: "Located on a military base you will have camping, grilling and fun in the water with many beach activities including kayaks and jet skis."
     }, {
       name: "Kerama Beach",
-      description: "info."
+      description: "This is the beach where you can swim with sea turtles. You will need a ferry to reach this beach."
     }, {
       name: "Sesoko Beach",
-      description: "Located at ANA Intercontinental Manza Beach Resort this family friendly beach has rentable water activites, including a fun playground for kids and adults!"
+      description: "No camping here but you can snorkel and relax at this 800-meter-long white smooth sandy beach with crystal-clear water. Located at Sesoko 73 Kunigamigun Motobucho."
     }, {
-      name: "Kabira Bay Beach",
-      description: "Any input I want"
+      name: "Emerald Beach",
+      description: "Located within Okinawa Ocean Expo Park in Motobu-cho. Relax and enjoy the sun with no admission fees."
     }, {
       name: "Zanpa Beach",
-      description: "add more info"
+      description: "Located just before Cape Zanpa, with plenty of water activites. Feel the sand between your toes! Pure white sand, beautiful blue water with no rocks to step on."
     }];
 
     //getting the beach image and applying a description
-    let items = document.querySelectorAll(".caption")
+    let items = document.querySelectorAll("#caption")
     let counter = 0
     for (let item of items){
       item.innerHTML = beaches[counter].description
